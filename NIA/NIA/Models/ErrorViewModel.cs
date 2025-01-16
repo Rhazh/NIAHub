960b9b736054f219211158bd081e58382f894c9a
Francis Osei Bonsu <subfrancis@gmail.com>
2025-01-16 18:27:42 -0500

    del

--- NIA/NIA/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NIA.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
