44f5c8157c721f5e9d302ad8b0067274fce1b534
Arianne T. <ariannetagorda@gmail.com>
2025-01-16 23:44:45 -0500

    Initial Project Files

---
+++ NIAHub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NIAHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
